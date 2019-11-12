e08dda4a02ce66583cad10b84debdd53329e56ac
yonatanahi <55342512+yonatanahi@users.noreply.github.com>
2019-11-12 19:42:12 +0200

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,3 @@ import { template } from "handlebars"
 const render = new Renderer()
 const trip = new TripManager()
 
-// var input = document.getElementById('autocomplete');
-// var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']});
-// google.maps.event.addListener(autocomplete, 'place_changed', function(){
-//    var place = autocomplete.getPlace();
-// })
