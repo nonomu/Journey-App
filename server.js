421cc09f7513f62ed18628fba780293e70b4c605
yonatanahi <55342512+yonatanahi@users.noreply.github.com>
2019-11-12 19:43:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const api = require('./server/routes/api')
 const bodyParser=require('body-parser')
 const basicDB= require('./server/models/Favorites')
 
-// const new1=new basicDB({CityName:"Noam"})
-// new1.save()
+const new1=new basicDB({CityName:"Noam"})
+new1.save()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// Mongoose setup
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Trip', { useNewUrlParser: true ,useUnifiedTopology: true})
+Mongoose setup
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Trip', { useNewUrlParser: true ,useUnifiedTopology: true})
 
 app.use(express.static(path.join(__dirname,  './dist')))
 app.use(express.static(path.join(__dirname,  './node_modules')))
