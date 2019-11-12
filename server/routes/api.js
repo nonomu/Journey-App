10890ad55e42e75da969dc91211f78ff1c0daba1
yonatanahi <55342512+yonatanahi@users.noreply.github.com>
2019-11-12 22:07:26 +0200

    fixing the scripts paths

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,10 +12,12 @@ const Favorites = require("../models/Favorites")
 router.get('/places/:cityName', async function(req, res){
     // getting city id for the next request
     let cityName = req.params.cityName
-    await requestPromise(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${cityName}&inputtype=textquery&fields=formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types&key=AIzaSyD_D-FODJApGj4CUB_V-ey9xzRH-gU2uRk`,
+    await requestPromise(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${cityName}&inputtype=textquery&fields=formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types&key=AIzaSyD_D-FODJApGj4CUB_V-ey9xzRH-gU2uRk&language=EN`,
 
     function(err, result){           
         result = JSON.parse(result.body)
+        console.log(result);
+        
         latitude = result.candidates[0].geometry.location.lat
         longitude = result.candidates[0].geometry.location.lng
         requestPromise(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&key=AIzaSyD_D-FODJApGj4CUB_V-ey9xzRH-gU2uRk&pagetoken`,
