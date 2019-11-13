d1f648fca41db3303262c74c4a67811f043cafb8
Your Name <you@example.com>
2019-11-13 12:45:24 +0200

    _stringForAPI method was added

--- dist/TripManager.js
+++ dist/TripManager.js
@@ -3,10 +3,28 @@ class TripManager {
     constructor() {
         this.tripData = []
     }
+    _stringForAPI(string) {
+        const brokenString = string.split(" ")
+        if (brokenString.length > 1) {
+            for (let word in brokenString) {
+                let upperCased = brokenString[word][0].toUpperCase() + brokenString[word].slice(1)
+                brokenString.splice(word, 1, upperCased)
+            }
 
+            string = brokenString.join(" ")
+        } else {
+            string = brokenString[0][0].toUpperCase() + brokenString[0].slice(1)
+        }
+        return string
+    }
 
-    async getCityWeather(destinationObject) {
-        let weather = await $.post("/cityWeather", destinationObject)
+    async getCityWeather(destination) {
+        let capDestination = {
+            cityName: this._stringForAPI(destination.cityName),
+            countryName: this._stringForAPI(destination.countryName)
+        }
+        console.log(capDestination)
+        let weather = await $.post("/cityWeather", capDestination)
         return weather
     }
 
