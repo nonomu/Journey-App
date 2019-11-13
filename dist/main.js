09e20455f9599216957bbb7b0562af539ce3791e
yonatanahi <55342512+yonatanahi@users.noreply.github.com>
2019-11-13 20:34:21 +0200

    adding place-auto complete

--- dist/main.js
+++ dist/main.js
@@ -30,3 +30,11 @@ $("#sites").on("click",".fa-plus-circle",function(){
     
     tripManager.addToFavorites(destination,site)
 })
+
+var input = document.getElementById('autocomplete');
+var autocomplete = new google.maps.places.Autocomplete(input,{types: ['(cities)']});
+google.maps.event.addListener(autocomplete, 'place_changed', function(){
+   var place = autocomplete.getPlace();
+   console.log(place.formatted_address);
+   
+})
